736e30e48a804691af620d19b1671843c2673cb2
Timothy Escopete #local #osxy <me@timothyae.com>
2015-10-19 01:20:54 +0800

    Rm EV perl, aliases, and exports; wtf;

--- oh-my-zsh/custom/timothy.zsh
+++ oh-my-zsh/custom/timothy.zsh
@@ -17,17 +17,6 @@ ZSH_THEME="timothy"
 # For scripting
 export PATH="$PATH:$HOME/.rvm/bin"
 
-# Perl churva
-PERL_MB_OPT="--install_base \"/Users/timothy/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/Users/timothy/perl5"; export PERL_MM_OPT;
-
-# EV Aliases
-alias composer="php /usr/bin/composer.phar"
-alias behat="php /vw/elephantventures/agency-environment/elephantventures/sites/all/tests/vendor/behat/behat/bin/behat"
-
-# EV Exports
-#source ~/.evrc
-
 # Live clocks
 alias clock1='while true; do tput clear; date +"                                                                                                         %l : %M : %S %p" | figlet -c | toilet -F gay -f term; sleep 1; done'
 alias clock2='while true; do tput clear; date +" %l : %M : %S %p" | toilet -F gay -f smmono9; sleep 1; done'
