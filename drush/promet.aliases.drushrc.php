9dfb169064fcb9898950c770f9d8d08f86ab1546
Timothy Escopete #local #osxec <me@timothyae.com>
2016-05-11 16:06:04 +0800

    Update promet drush aliases with instruction to turn on Remote Login, and added prompt to to yes or no, to give time to check it out.

--- drush/promet.aliases.drushrc.php
+++ drush/promet.aliases.drushrc.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Promet local vagrant drush aliases.
  */
@@ -25,14 +24,38 @@ $param1 = $temp[1];
 // Prompt password is asking for authorize.
 if ($param1 == 'authorize') {
   drush_print('');
-  drush_print('Please supply the password for your host machine.');
+  drush_print('Make sure you turned on \'Remote Login\' in your System Preferences\' Sharing');
+  drush_print('To turn it on, go to  \'System Preferences > Sharing\'  and mark checked the');
+  drush_print('\'Remote Login\'.');
+  drush_print('');
+  drush_print('This is just a one time request, and \'Remote Login\' can be turned back off.');
+  drush_print('');
+  drush_print('If you are done, confirm it below.');
   drush_print('');
 
-  $aliases[$project] = array(
-    'root' => '/',
-    'uri' => '127.0.0.1',
-    'remote-host' => '127.0.0.1',
+  $proceed = drush_choice(
+    array(
+      'yes' => 'Yes',
+    ),
+    'Do you want to proceed? '
   );
+
+  if ($proceed == 'yes') {
+    drush_print('');
+    drush_print('Please supply the password for your host machine.');
+    drush_print('');
+
+    $aliases[$project] = array(
+      'root' => '/',
+      'uri' => '127.0.0.1',
+      'remote-host' => '127.0.0.1',
+    );
+  }
+  else {
+    drush_print('');
+    drush_set_error('Aborted', 'Request aborted.');
+    exit();
+  }
 }
 else {
   // Generic template to vagrant.
