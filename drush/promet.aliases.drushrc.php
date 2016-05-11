68e4c517680c49f8d8e151c083a0db7ae36a0bb7
Timothy Escopete #local #osxec <me@timothyae.com>
2016-05-12 03:30:28 +0800

    Remove lengthy comment(s) from the drushrc.php file.

--- drush/promet.aliases.drushrc.php
+++ drush/promet.aliases.drushrc.php
@@ -2,53 +2,8 @@
 /**
  * Promet's dynamic drush aliases.
  *
- * **********
- * PARAMETERS
- * **********
- * As used in this drushrc file.
- *
- * $env:
- *  - This can be `local`, `dev`, or `stage`.
- *  - A null or empty value is considered `local` by default.
- *
- * $project:
- *  - Project name, e.g. martincounty.
- *  - Project name can be obtained from either:
- *      a) The value of `project` from your project's Vagrantfile, or
- *      b) Just trip away `.dev` from the url of your working local environment.
- *           For example, the local url for Martin County is `martincounty.dev`.
- *           The project name would then be `martincounty`.
- *
- * $param1:
- *  - Promet's defined shell aliases to do commonly recognized shell commands
- *      inside the server.  For example, `authorize`.
- *
- * ******
- * FORMAT
- * ******
- * Legend:
- *  - The use of { and } means, it is a variable.
- *  - The use of [ and ] means, it is optional.
- *
- * Format:
- * drush @promet.{project}[.{env}] {drush-command(s)}
- *
- * {drush-command(s)} can be any drush command(s) or any custom drush shell
- *    aliases defined by Promet.
- *
- * *******
- * EXAMPLE
- * *******
- * Based on the given format above, here's the example for Martin County.
- *
- * drush @promet.martincounty st          - Execute drush in local vagrant.
- *
- * drush @promet.martincounty.local st    - `.local` can be ommitted to execute
- *                                            drush in local vagrant.
- *
- * drush @promet.martincounty.dev st      - Execute drush in Promet Dev server.
- *
- * drush @promet.martincounty.stage st    - Execute drush in Promet Stage server.
+ * Please see README.md file for more info.
+ * /timhtheos
  */
 
 // Get command.
