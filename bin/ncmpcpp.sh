8fb031ae490a050fbde28eec5463723bf719a81c
timhtheos <me@timothyae.com>
2016-09-08 07:29:24 +0800

    Update ncmpcpp.sh file.

--- bin/ncmpcpp.sh
+++ bin/ncmpcpp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-t update "Now listening to $(mpc current) #$(mpc current --format [%genre%])" 1>/dev/null
+# t update "Now listening to $(mpc current) #$(mpc current --format [%genre%])" 1>/dev/null
+# t reply 741937740405579777 "@wretchedsaved Now listening to $(mpc current) #$(mpc current --format [%genre%])" &> /dev/null
